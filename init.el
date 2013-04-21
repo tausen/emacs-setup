115a23264f5a53fadc55af43a290a5499b44cab5
Mathias Tausen <mathias@innowell.net>
2013-04-21 23:22:40 +0200

    Added web stuff, minor

--- init.el
+++ init.el
@@ -189,7 +189,8 @@
     (kill-line arg)))
 (global-set-key [(control shift ?k)] 'delete-line) 
 
-;; toggle truncate lines hotkey
+;; truncate lines 
+(setq-default truncate-lines t)
 (global-set-key (kbd "<f6>") 'toggle-truncate-lines)
 
 ;; bash autocompletion in emacs shell
@@ -401,6 +402,116 @@ search modes defined in the new `dired-sort-toggle'.
 
 ;;; dired-fixups.el ends here
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;;;;;; WEB ;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defun web/trim (str)
+  (replace-regexp-in-string "^[[:space:]]*\\|[[:space:]]*$" "" str))
+
+
+(defun web/css-propertyp (property)
+  "Checks if something can be considered a CSS property."
+  (or (string= "-" (substring property 0 1))
+      (member (downcase property) css-property-ids)))
+
+
+(defun web/css-prettify-selectors (selectors)
+  "Returns a properly sorted string with selectors."
+  (concat (replace-regexp-in-string " *, *"
+                                    (concat ",\n")
+                                    selectors)
+          " {"))
+
+
+(defun web/css-fix-property-spacing ()
+  "Converts things like 'color:x' to `color: x'.
+
+It expects a properly indented CSS"
+  (interactive)
+  (save-excursion)
+  (goto-char (point-min))
+  (while (re-search-forward "^ *\\([^:]+\\) *: *" nil t)
+    (if (web/css-propertyp (match-string 1))
+        (replace-match (concat "    "
+                               (match-string 1)
+                               ": ")))))
+
+
+(defun web/css-indent-buffer ()
+  "Indents the whole buffer correctly."
+  (interactive)
+  (indent-region (point-min) (point-max)))
+
+
+(defun web/css-unminify ()
+  "Unminifies the whole CSS."
+  (interactive)
+  (save-excursion
+    (goto-char (point-min))
+    (while (re-search-forward "{" nil t)
+      (replace-match "{\n")))
+  (save-excursion
+    (goto-char (point-min))
+    (while (re-search-forward "}" nil t)
+      (replace-match "\n}\n")))
+  (save-excursion
+    (goto-char (point-min))
+    (while (re-search-forward ";" nil t)
+      (replace-match ";\n")))
+  (web/css-indent-buffer)
+  (web/css-align-properties))
+
+
+(defun web/css-align-properties ()
+  "Aligns the properties with align-region"
+  (interactive)
+  (align-regexp (point-min) (point-max) "^ +\\([^:]+ *\\)\\(: *\\) " 2 1 nil))
+
+
+(defun web/css-fix-selectors ()
+  "Uses one selector per line"
+  (interactive)
+  (save-excursion)
+  (goto-char (point-min))
+  (while (re-search-forward "^\\(.+\\) *{ *" nil t)
+    (replace-match (web/css-prettify-selectors (match-string 1)))))
+
+
+(defun web/css-fix-brace-spacing ()
+  "Fixes brace spacing"
+  (interactive)
+  (save-excursion)
+  (goto-char (point-min))
+  (while (re-search-forward " *{ *$" nil t)
+    (replace-match " {")))
+
+
+(defun web/css-remove-semicolons ()
+  "Removes semicolons from the end of the line to make it moar clean."
+  (interactive)
+  (save-excursion)
+  (goto-char (point-min))
+  (while (re-search-forward " *; *$" nil t)
+    (replace-match "")))
+
+
+(defun web/css-fix-formatting ()
+  "Fixes all the formatting in the file."
+  (interactive)
+  (web/css-fix-selectors)
+  (web/css-indent-buffer)
+  (web/css-fix-property-spacing)
+  (web/css-fix-brace-spacing)
+  (web/css-align-properties))
+
+
+
+(defun web/css->stylus ()
+  "Converts a css file to stylus"
+  (interactive)
+  (web/css-fix-formatting)
+  (web/css-remove-semicolons))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 
 
