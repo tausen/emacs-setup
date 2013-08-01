9bb95bf3d01c4256332bc72a1836322abfb9f2a5
Mathias Tausen <mathias@innowell.net>
2013-08-01 21:50:27 +0200

    Bound F2 to create/rename ansi term buffers

--- init.el
+++ init.el
@@ -104,11 +104,13 @@
       (forward-line -1)
       (c-indent-line))))
 
+(define-key global-map (kbd "C-{") 'my-c-mode-insert-lcurly)
+
 ; NEEDED IN NXHTML MODE
 (add-hook 'php-mode-hook
           '(lambda ()
              (local-set-key ";" 'self-insert-command)
-             (local-set-key "{" 'my-c-mode-insert-lcurly)))
+             (local-set-key "{" 'self-insert-command)))
 
 
 ;; fgallina/multi-web-mode - https://github.com/fgallina/multi-web-mode
@@ -692,3 +694,32 @@ It expects a properly indented CSS"
  (global-set-key "\C-xm" 'browse-url-at-point)
 (setq w3m-use-cookies t)
 
+;; rename ansi-term buffer
+(require 'term)
+(defun visit-ansi-term ()
+  "If the current buffer is:
+     1) a running ansi-term named *ansi-term*, rename it.
+     2) a stopped ansi-term, kill it and create a new one.
+     3) a non ansi-term, go to an already running ansi-term
+        or start a new one while killing a defunt one"
+  (interactive)
+  (let ((is-term (string= "term-mode" major-mode))
+        (is-running (term-check-proc (buffer-name)))
+        (term-cmd "/bin/bash")
+        (anon-term (get-buffer "*ansi-term*")))
+    (if is-term
+        (if is-running
+            (if (string= "*ansi-term*" (buffer-name))
+                (call-interactively 'rename-buffer)
+              (if anon-term
+                  (switch-to-buffer "*ansi-term*")
+                (ansi-term term-cmd)))
+          (kill-buffer (buffer-name))
+          (ansi-term term-cmd))
+      (if anon-term
+          (if (term-check-proc "*ansi-term*")
+              (switch-to-buffer "*ansi-term*")
+            (kill-buffer "*ansi-term*")
+            (ansi-term term-cmd))
+        (ansi-term term-cmd)))))
+(global-set-key (kbd "<f2>") 'visit-ansi-term)
