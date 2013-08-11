c3644cacc993690ec6318a305bc1ec370fbe5c62
Mathias Tausen <mathias.tausen@gmail.com>
2013-08-11 21:07:16 +0200

    Bfin mode // are also comments

--- init.el
+++ init.el
@@ -555,15 +555,16 @@ It expects a properly indented CSS"
 
 ;; Bfin assembly mode
 (define-generic-mode 
-    'bfin-mode                      ; name of the mode to create
-  '("#")                            ; comments start with '!!'
-  '("LSETUP" "RTS")       ; some keywords
-  '(("=" . 'font-lock-operator)     ; '=' is an operator
-    (";" . 'font-lock-builtin)     ; ';' is a built-in
-    ("\\(^\s*\\.[a-zA-Z0-9]+:?\\)" . 'font-lock-keyword-face) ; labels: .something:
+    'bfin-mode                          ; name of the mode to create
+  '(("#")
+    ("//"))                             ; comments start with '#' or '//'
+  '("LSETUP" "RTS" "LINK")              ; some keywords
+  '(("=" . 'font-lock-operator)         ; '=' is an operator
+    (";" . 'font-lock-builtin)          ; ';' is a built-in
+    ("\\(^\s*\\.[a-zA-Z0-9_]+:?\\)" . 'font-lock-keyword-face) ; labels: .something:
     ("\\(\s+0[xX][0-9a-fA-F]+\\)" . 'font-lock-variable-name-face) ; hex numbers
     ("\\(\s+[0-9a-f]+\\)" . 'font-lock-variable-name-face) ; decimal numbers
-    ("\\(^\s*_[a-zA-Z0-9]+:?\\)" . 'font-lock-function-name-face)) ; _something:
+    ("\\(^\s*_[a-zA-Z0-9]+:?\\)" . 'font-lock-function-name-face)) ; symbols: _something:
   '("\\.s$")                        ; files for which to activate this mode 
   nil                               ; other functions to call
   "A mode for bfin assembly"        ; doc string for this mode
