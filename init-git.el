19b823a312208ff0fb3e37a7a9438dc99235857d
Mathias Tausen <mathias.tausen@gmail.com>
2013-11-08 09:15:58 +0100

    init-git (magit)

--- init-git.el
+++ init-git.el
@@ -1,4 +1,5 @@
 ;; git plugin
+(add-to-list 'load-path "~/.emacs.d/lib/magit-1.2.0/")
 (require 'magit)
 (global-set-key (kbd "C-c g") 'magit-status)
 ; (magit-status-buffer-switch-function (quote switch-to-buffer))
