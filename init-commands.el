c968b0840d16d5f6baf684bdfd0a1782d3a458e7
Mathias Tausen <mathias.tausen@gmail.com>
2013-11-10 11:29:23 +0100

    added hotkey to toggle fullscreen

--- init-commands.el
+++ init-commands.el
@@ -74,3 +74,14 @@
   (delete-trailing-whitespace)
   (message "Deleted all trailing whitespace"))
 (global-set-key (kbd "C-c w") 'my-delete-trailing-whitespace)
+
+;; toggle fullscreen
+(defun toggle-fullscreen ()
+  "Toggle full screen on X11"
+  (interactive)
+  (when (eq window-system 'x)
+    (set-frame-parameter
+     nil 'fullscreen
+     (when (not (frame-parameter nil 'fullscreen)) 'fullboth))))
+
+(global-set-key [f11] 'toggle-fullscreen)
