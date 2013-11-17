a7caa83dcec90408c4d638a8cd37a54812bd2986
Mathias Tausen <mathias@innowell.net>
2013-11-17 21:30:10 +0100

    hotfix to mumamo warning spam

--- init-web.el
+++ init-web.el
@@ -160,3 +160,13 @@ It expects a properly indented CSS"
 (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
 ;; because nxhtml-mode forces loading .js files with javascript-mode (http://goo.gl/uc9aCl)
 (defalias 'javascript-mode 'js2-mode)
+
+;; https://gist.github.com/tkf/3951163
+;; Workaround the annoying warnings:
+;;    Warning (mumamo-per-buffer-local-vars):
+;;    Already 'permanent-local t: buffer-file-name
+(when (and (>= emacs-major-version 24)
+           (>= emacs-minor-version 2))
+  (eval-after-load "mumamo"
+    '(setq mumamo-per-buffer-local-vars
+           (delq 'buffer-file-name mumamo-per-buffer-local-vars))))
