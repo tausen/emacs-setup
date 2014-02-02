164ac8322a5d333465d4f92d3a86f5fcdfd85fec
Mathias Tausen <mathias.tausen@gmail.com>
2014-02-02 18:56:58 +0100

    Added emacs markdown-mode

--- init.el
+++ init.el
@@ -38,6 +38,7 @@
 (load "~/.emacs.d/init-latex.el")
 (load "~/.emacs.d/init-git.el")
 (load "~/.emacs.d/init-w3m.el")
+(load "~/.emacs.d/init-markdown.el")
 
 ;; auto-complete
 (add-to-list 'load-path "~/.emacs.d/lib/auto-complete/")
