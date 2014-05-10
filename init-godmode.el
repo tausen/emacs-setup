8ce6fec817eb6cbee6b2e346dea53d9e02221edb
Mathias Tausen <mathias.tausen@gmail.com>
2014-05-10 14:09:56 +0200

    Few changes to god-mode hotekys etc., now working properly without x11

--- init-godmode.el
+++ init-godmode.el
@@ -1,25 +1,26 @@
 (add-to-list 'load-path "~/.emacs.d/lib/god-mode")
 (require 'god-mode)
 
-(defun my-update-cursor ()
-  (setq cursor-type (if (or god-local-mode buffer-read-only)
-                        'bar
-                      'box)))
-
-(add-hook 'god-mode-enabled-hook 'my-update-cursor)
-(add-hook 'god-mode-disabled-hook 'my-update-cursor)
 
 (global-set-key (kbd "C-x C-1") 'delete-other-windows)
 (global-set-key (kbd "C-x C-2") 'split-window-below)
 (global-set-key (kbd "C-x C-3") 'split-window-right)
 (global-set-key (kbd "C-x C-0") 'delete-window)
+(global-set-key (kbd "C-x C-{") 'shrink-window-horizontally)
+(global-set-key (kbd "C-x C-}") 'enlarge-window-horizontally)
+(global-set-key (kbd "C-x C-[") 'shrink-window)
+(global-set-key (kbd "C-x C-]") 'enlarge-window)
+(global-set-key (kbd "C-x C-5 C-0") 'delete-frame)
+(global-set-key (kbd "C-x C-#") 'server-edit)
 
 (define-key god-local-mode-map (kbd "z") 'repeat)
 (define-key god-local-mode-map (kbd "i") 'god-mode-all)
 
 ;; Bind ALL the keys
 (global-set-key (kbd "<escape>") 'god-mode-all)
-(global-set-key (kbd "<RET>") 'god-mode-all)
+(global-set-key (kbd "<RET>") 'god-mode-all) ;; really bad when pasting into nowindow
+(global-set-key (kbd "<home>") 'god-mode-all)
+(global-set-key (kbd "<f12>") 'god-mode-all)
 (add-hook 'LaTeX-mode-hook (lambda () (local-set-key (kbd "<RET>") 'god-mode-all)))
 (add-hook 'org-mode-hook (lambda () (local-set-key (kbd "<RET>") 'god-mode-all)))
 
@@ -34,7 +35,6 @@
 ;; (define-key god-local-mode-map (kbd "s-k") 'windwmove-up)
 ;; (define-key god-local-mode-map (kbd "s-l") 'windwmove-left)
 ;; (define-key god-local-mode-map (kbd "s-h") 'windwmove-right)
-
 
 ;; (defun god-toggle-on-overwrite ()
 ;;   "Toggle god-mode on overwrite-mode."
@@ -53,3 +53,50 @@
 
 ;; could be used... obviously change f9 to something else
 ;(define-key key-translation-map (kbd "<f9>") 'event-apply-control-modifier)
+
+;; (defun c/god-mode-update-cursor ()
+;;   (let ((limited-colors-p (> 257 (length (defined-colors)))))
+;;     (cond (god-local-mode (progn
+;;                             (set-face-background 'mode-line (if limited-colors-p "blue4" "#8b1a1a"))
+;;                             (set-face-background 'mode-line-inactive (if limited-colors-p "gray25" "#404040"))))
+;;           (t (progn
+;;                (set-face-background 'mode-line (if limited-colors-p "gray15" "#262626"))
+;;                (set-face-background 'mode-line-inactive (if limited-colors-p "gray25" "#404040")))))))
+
+;; only works with x11
+(defun my-update-cursor ()
+  (setq cursor-type (if (or god-local-mode buffer-read-only)
+                        'bar
+                      'box)))
+(add-hook 'god-mode-enabled-hook 'my-update-cursor)
+(add-hook 'god-mode-disabled-hook 'my-update-cursor)
+
+;; indicate whether god-mode is on with blue mode-line background
+;; always works (if not in limited colors term..)
+(defun c/god-mode-update-cursor ()
+  (cond (god-local-mode (progn (set-face-background 'mode-line "blue4")))
+        (t (progn (set-face-background 'mode-line "gray15")))))
+(add-hook 'god-mode-enabled-hook 'c/god-mode-update-cursor)
+(add-hook 'god-mode-disabled-hook 'c/god-mode-update-cursor)
+
+
+;; toggle RET between god-mode-all and newline with f7
+(setq god-bound-to-ret t)
+
+(defun bind-ret-god ()
+  (cond
+
+   (god-bound-to-ret (global-set-key (kbd "<RET>") 'newline)
+                     (setq god-bound-to-ret nil)
+                     (message "RET bound to newline") )
+
+   (t (global-set-key (kbd "<RET>") 'god-mode-all)
+      (setq god-bound-to-ret t)
+      (message "RET bound to god-mode-all") )
+   
+   ))
+
+(global-set-key (kbd "<f7>") (lambda () (interactive) (bind-ret-god)))
+
+
+
