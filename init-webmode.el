35af0d5942396bb5f20ebc8e789462ee22892eab
Mathias Tausen <mathias.tausen@gmail.com>
2014-07-09 21:50:45 +0200

    Hotkey for php arrow

--- init-webmode.el
+++ init-webmode.el
@@ -17,3 +17,18 @@
 (autoload 'js2-mode "js2-mode" nil t)
 (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
 (defalias 'javascript-mode 'js2-mode)
+
+;; Hotkey for arrow (for php..)
+(defun insert-arrow ()
+  (interactive)
+  (insert "->"))
+
+(add-hook 'php-mode-hook (lambda () (local-set-key (kbd "C-c C-a") 'insert-arrow)))
+
+
+;; Insert custom web-mode hooks here
+(defun my-web-mode-hook ()
+  "Hooks for Web mode."
+  (local-set-key (kbd "C-c C-a") 'insert-arrow)
+)
+(add-hook 'web-mode-hook 'my-web-mode-hook)
