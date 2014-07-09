5f883edb10b6d8e1f9592efc9f871f3db417b3b5
Mathias Tausen <mathias.tausen@gmail.com>
2014-07-09 21:49:59 +0200

    Some comments for possible future changes in init-latex.el

--- init-latex.el
+++ init-latex.el
@@ -43,3 +43,30 @@
 ; (setq TeX-auto-save t)
 ; (setq TeX-parse-self t)
 ; (setq-default TeX-master nil)
+
+
+;; This is not working for some reason :/
+;; (defun guess-TeX-master (filename)
+;;   "Guess the master file for FILENAME from currently open .tex files."
+;;   (let ((candidate nil)
+;;         (filename (file-name-nondirectory filename)))
+;;     (save-excursion
+;;       (dolist (buffer (buffer-list))
+;;         (with-current-buffer buffer
+;;           (let ((name (buffer-name))
+;;                 (file buffer-file-name))
+;;             (if (and file (string-match "\\.tex$" file))
+;;                 (progn
+;;                   (goto-char (point-min))
+;;                   (if (re-search-forward (concat "\\\\input{" filename "}") nil t)
+;;                       (setq candidate file))
+;;                   (if (re-search-forward (concat "\\\\include{" (file-name-sans-extension filename) "}") nil t)
+;;                       (setq candidate file))))))))
+;;     (if candidate
+;;         (message "TeX master document: %s" (file-name-nondirectory candidate)))
+;;     candidate))
+
+;; (add-hook 'latex-mode-hook (lambda () (setq TeX-master (guess-TeX-master (buffer-file-name)))))
+
+;; Change fontsize in latex previews
+(set-default 'preview-scale-function 1.0)
