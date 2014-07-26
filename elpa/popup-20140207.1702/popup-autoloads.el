f0ebad80ed8b08d49103a000b0e550913b8e0914
Mathias Tausen <mathias.tausen@gmail.com>
2014-07-26 18:57:43 +0200

    Add auto-complete, fuzzy and popup (installed via elpa)

---
+++ elpa/popup-20140207.1702/popup-autoloads.el
@@ -0,0 +1,18 @@
+;;; popup-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("popup-pkg.el" "popup.el") (21459 51382
+;;;;;;  861703 996000))
+
+;;;***
+
+(provide 'popup-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; popup-autoloads.el ends here
