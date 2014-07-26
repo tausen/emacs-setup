1e2eb4db6472b64e42d3aed09a724842a32b5938
Mathias Tausen <mathias.tausen@gmail.com>
2014-07-26 17:54:02 +0200

    Don't enable projectile globally by default (slows down emacs while working via sshfs)

--- init-projectile.el
+++ init-projectile.el
@@ -21,7 +21,7 @@
 (add-to-list 'load-path "~/emacs.d/lib/projectile")
 (load "~/.emacs.d/lib/projectile/projectile.el")
 (require 'projectile)
-(projectile-global-mode)
+;; (projectile-global-mode)
 
 ;; Allows C-c p R to recompile tags
 ;; my ctags did not recognize the -e option, so had to install
