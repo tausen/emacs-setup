3cc05790e32a43dfaeb4380ba0d4c52f117471bb
Nicklas Andersen <figa.figa@gmail.com>
2014-09-23 13:28:59 +0200

    Added TypeScript mode and TypeScript intellisense

---
+++ init-tss.el
@@ -0,0 +1,16 @@
+;; If use bundled typescript.el,
+(require 'typescript)
+(add-to-list 'auto-mode-alist '("\\.ts\\'" . typescript-mode))
+
+(require 'tss)
+
+;; Key binding
+(setq tss-popup-help-key "C-:")
+(setq tss-jump-to-definition-key "C->")
+(setq tss-implement-definition-key "C-c i")
+
+;; Make config suit for you. About the config item, eval the following sexp.
+;; (customize-group "tss")
+
+;; Do setting recommemded configuration
+(tss-config-default)
