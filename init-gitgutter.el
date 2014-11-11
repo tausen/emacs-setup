c27b4e6572daad1e51858135fc595a7f9edd1b02
Mathias Tausen <mathias.tausen@gmail.com>
2014-11-11 21:05:35 +0100

    Dont configure git gutter when running emacs in terminal

--- init-gitgutter.el
+++ init-gitgutter.el
@@ -1,34 +1,36 @@
+;; load and configure git-gutter fringe plus if not running in terminal
+(when window-system
+  (add-to-list 'load-path "~/.emacs.d/lib/git-gutter-plus")
+  (add-to-list 'load-path "~/.emacs.d/lib/git-gutter-fringe-plus")
 
+  (require 'git-gutter+)
+  (require 'git-gutter-fringe+)
 
-(add-to-list 'load-path "~/.emacs.d/lib/git-gutter-plus")
-(add-to-list 'load-path "~/.emacs.d/lib/git-gutter-fringe-plus")
+  ;; neutral ish colors (for zenburn at least)
+  (set-face-attribute 'git-gutter-fr+-added nil :foreground "#5b8258")
+  (set-face-attribute 'git-gutter-fr+-deleted nil :foreground "#965555")
+  (set-face-attribute 'git-gutter-fr+-modified nil :foreground "#936b91")
 
-(require 'git-gutter+)
-(require 'git-gutter-fringe+)
+  (global-git-gutter+-mode t)
 
-(global-git-gutter+-mode t)
+  ;; (git-gutter-fr+-minimal) ;; <- minimal theme, grayish colors
 
-;; (git-gutter-fr+-minimal) ;; <- minimal theme, grayish colors
-
-;; neutral ish colors (for zenburn at least)
-(set-face-attribute 'git-gutter-fr+-added nil :foreground "#5b8258")
-(set-face-attribute 'git-gutter-fr+-deleted nil :foreground "#965555")
-(set-face-attribute 'git-gutter-fr+-modified nil :foreground "#936b91")
-
-;; hotkeys
-(eval-after-load 'git-gutter+
-  '(progn
+  ;; hotkeys
+  (eval-after-load 'git-gutter+
+    '(progn
      ;;; Jump between hunks
-     (define-key git-gutter+-mode-map (kbd "C-c C-v C-n") 'git-gutter+-next-hunk)
-     (define-key git-gutter+-mode-map (kbd "C-c C-v C-p") 'git-gutter+-previous-hunk)
+       (define-key git-gutter+-mode-map (kbd "C-c C-v C-n") 'git-gutter+-next-hunk)
+       (define-key git-gutter+-mode-map (kbd "C-c C-v C-p") 'git-gutter+-previous-hunk)
 
      ;;; Act on hunks
-     (define-key git-gutter+-mode-map (kbd "C-c C-v C-=") 'git-gutter+-show-hunk)
-     (define-key git-gutter+-mode-map (kbd "C-c C-v C-r") 'git-gutter+-revert-hunks)
-     ;; Stage hunk at point.
-     ;; If region is active, stage all hunk lines within the region.
-     (define-key git-gutter+-mode-map (kbd "C-c C-v C-s") 'git-gutter+-stage-hunks)
-     (define-key git-gutter+-mode-map (kbd "C-c C-v C-c") 'git-gutter+-commit)
-     ;; (define-key git-gutter+-mode-map (kbd "C-c C-v C-C") 'git-gutter+-stage-and-commit)
-     (define-key git-gutter+-mode-map (kbd "C-c C-v C-b") 'git-gutter+-stage-and-commit-whole-buffer)
-     (define-key git-gutter+-mode-map (kbd "C-c C-v C-u") 'git-gutter+-unstage-whole-buffer)))
+       (define-key git-gutter+-mode-map (kbd "C-c C-v C-=") 'git-gutter+-show-hunk)
+       (define-key git-gutter+-mode-map (kbd "C-c C-v C-r") 'git-gutter+-revert-hunks)
+       ;; Stage hunk at point.
+       ;; If region is active, stage all hunk lines within the region.
+       (define-key git-gutter+-mode-map (kbd "C-c C-v C-s") 'git-gutter+-stage-hunks)
+       (define-key git-gutter+-mode-map (kbd "C-c C-v C-c") 'git-gutter+-commit)
+       ;; (define-key git-gutter+-mode-map (kbd "C-c C-v C-C") 'git-gutter+-stage-and-commit)
+       (define-key git-gutter+-mode-map (kbd "C-c C-v C-b") 'git-gutter+-stage-and-commit-whole-buffer)
+       (define-key git-gutter+-mode-map (kbd "C-c C-v C-u") 'git-gutter+-unstage-whole-buffer)))
+
+  )
