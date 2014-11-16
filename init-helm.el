3f49c57799220d4da7fed67123100559d86795f9
Mathias Tausen <mathias.tausen@gmail.com>
2014-11-16 17:09:47 +0100

    Helm projectile

--- init-helm.el
+++ init-helm.el
@@ -2,23 +2,23 @@
 (add-to-list 'load-path "~/.emacs.d/lib/helm")
 (require 'helm-config)
 
-;; (load "~/.emacs.d/lib/projectile/helm-projectile.el")
-;; (require 'helm-projectile)
+(load "~/.emacs.d/lib/projectile/helm-projectile.el")
+(require 'helm-projectile)
 
 (helm-mode 1)
-;; (helm-projectile-on)
+(helm-projectile-on)
 
 (global-set-key (kbd "C-c C-h") 'helm-mini)
 (global-set-key (kbd "M-x") 'helm-M-x)
 (global-set-key (kbd "C-x C-f") 'helm-find-files)
 (global-set-key (kbd "C-x C-b") 'helm-buffers-list)
-;; (global-set-key (kbd "C-c C-p C-h") 'helm-projectile)
-;; (global-set-key (kbd "C-c C-p C-f") 'helm-projectile-find-file)
-;; (global-set-key (kbd "C-c C-p C-p") 'helm-projectile-switch-project)
+(global-set-key (kbd "C-c C-p C-h") 'helm-projectile)
+(global-set-key (kbd "C-c C-p C-f") 'helm-projectile-find-file)
+(global-set-key (kbd "C-c C-p C-p") 'helm-projectile-switch-project)
 
 ;; override hotkey in python-mode
-;; (add-hook 'python-mode-hook (lambda () (local-unset-key (kbd "C-c C-p"))))
-;; (add-hook 'python-mode-hook (lambda () (local-set-key (kbd "C-c C-p C-h") 'helm-projectile)))
+(add-hook 'python-mode-hook (lambda () (local-unset-key (kbd "C-c C-p"))))
+(add-hook 'python-mode-hook (lambda () (local-set-key (kbd "C-c C-p C-h") 'helm-projectile)))
 
 ;; Bind smex M-x to C-c M-x
 (add-to-list 'load-path "~/.emacs.d/lib/smex/")
