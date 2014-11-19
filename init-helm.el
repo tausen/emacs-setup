1eabe512d4087493cc5c0df5b4fb2cf65878b3b1
Mathias Tausen <mathias.tausen@gmail.com>
2014-11-19 14:55:04 +0100

    Few more changes to helm config

--- init-helm.el
+++ init-helm.el
@@ -2,23 +2,14 @@
 (add-to-list 'load-path "~/.emacs.d/lib/helm")
 (require 'helm-config)
 
-(load "~/.emacs.d/lib/projectile/helm-projectile.el")
-(require 'helm-projectile)
-
 (helm-mode 1)
-(helm-projectile-on)
 
-(global-set-key (kbd "C-c C-h") 'helm-mini)
+(global-set-key (kbd "C-c h") 'helm-mini)
 (global-set-key (kbd "M-x") 'helm-M-x)
 (global-set-key (kbd "C-x C-f") 'helm-find-files)
 (global-set-key (kbd "C-x C-b") 'helm-buffers-list)
-(global-set-key (kbd "C-c C-p C-h") 'helm-projectile)
-(global-set-key (kbd "C-c C-p C-f") 'helm-projectile-find-file)
-(global-set-key (kbd "C-c C-p C-p") 'helm-projectile-switch-project)
-
-;; override hotkey in python-mode
-(add-hook 'python-mode-hook (lambda () (local-unset-key (kbd "C-c C-p"))))
-(add-hook 'python-mode-hook (lambda () (local-set-key (kbd "C-c C-p C-h") 'helm-projectile)))
+(global-set-key (kbd "C-c C-x C-b") 'ibuffer)
+(global-set-key (kbd "C-c s h") 'helm-do-grep)
 
 ;; Bind smex M-x to C-c M-x
 (add-to-list 'load-path "~/.emacs.d/lib/smex/")
@@ -26,3 +17,7 @@
 (smex-initialize) ; Can be omitted. This might cause a (minimal) delay
                   ; when Smex is auto-initialized on its first run.
 (global-set-key (kbd "C-c M-x") 'smex)
+
+(load "~/.emacs.d/lib/projectile/helm-projectile.el")
+(require 'helm-projectile)
+(helm-projectile-on)
