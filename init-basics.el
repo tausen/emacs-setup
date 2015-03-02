dab1d2a4fda992fb788bb2eccb0de08994584fe5
Mathias Tausen <mathias.tausen@gmail.com>
2015-03-02 23:07:32 +0100

    Bind comment-or-uncomment-region-or-line to M-' rather than M-[
    Turns out that binding stuff to M-[ conflicts with the function
    keys (f1, f2, ..) when running emacs with nowindow

--- init-basics.el
+++ init-basics.el
@@ -219,7 +219,7 @@
             (setq beg (region-beginning) end (region-end))
             (setq beg (line-beginning-position) end (line-end-position)))
         (comment-or-uncomment-region beg end)))
-(global-set-key (kbd "M-[") 'comment-or-uncomment-region-or-line)
+(global-set-key (kbd "M-'") 'comment-or-uncomment-region-or-line)
 
 ;; delete pair of parens, ..
 (global-set-key (kbd "C-x p") 'delete-pair)
