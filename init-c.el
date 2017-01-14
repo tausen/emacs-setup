98f36809915e410a28f7668ea076260e66befd32
Mathias Tausen <mta@gomspace.com>
2017-01-14 16:21:16 +0100

    Minor cleanup

--- init-c.el
+++ init-c.el
@@ -15,36 +15,3 @@
 ;; google c style
 (load "~/.emacs.d/lib/google-c-style.el")
 (add-hook 'c-mode-common-hook 'google-set-c-style)
-
-;; TODO
-;; from https://www.kernel.org/doc/Documentation/CodingStyle
-;; (defun c-lineup-arglist-tabs-only (ignored)
-;;   "Line up argument lists by tabs, not spaces"
-;;   (let* ((anchor (c-langelem-pos c-syntactic-element))
-;;          (column (c-langelem-2nd-pos c-syntactic-element))
-;;          (offset (- (1+ column) anchor))
-;;          (steps (floor offset c-basic-offset)))
-;;     (* (max steps 1)
-;;        c-basic-offset)))
-
-;; (add-hook 'c-mode-common-hook
-;;           (lambda ()
-;;             ;; Add kernel style
-;;             (c-add-style
-;;              "linux-tabs-only"
-;;              '("gomspace" (c-offsets-alist
-;;                            (arglist-cont-nonempty
-;;                             c-lineup-gcc-asm-reg
-;;                             c-lineup-arglist-tabs-only))))))
-
-;; (add-hook 'c-mode-hook
-;;           (lambda ()
-;;             (let ((filename (buffer-file-name)))
-;;               ;; Enable kernel mode for the appropriate files
-;;               (when (and filename
-;;                          (string-match (expand-file-name "~/src/")
-;;                                        filename))
-;;                 (setq indent-tabs-mode t)
-;;                 (setq show-trailing-whitespace t)
-;;                 (c-set-style "linux-tabs-only")))))
-;; #########################
