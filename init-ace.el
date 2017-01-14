103cdefaa4b087811b1f0c1b116c830c6bc6a706
Mathias Tausen <mta@gomspace.com>
2017-01-14 15:46:07 +0100

    Deleting stuff

--- init-ace.el
+++
@@ -1,9 +0,0 @@
-;; ace jump mode major function
-(add-to-list 'load-path "~/.emacs.d/lib/ace-jump-mode")
-(autoload
-  'ace-jump-mode
-  "ace-jump-mode"
-  "Emacs quick move minor mode"
-  t)
-(define-key global-map (kbd "C-c SPC") 'ace-jump-mode)
-(add-hook 'org-mode-hook (lambda () (local-set-key (kbd "C-c SPC") 'ace-jump-mode)))
