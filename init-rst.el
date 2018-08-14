b12646bf3e65135c897544abce4c80f6c3159e0e
Mathias Tausen <mta@gomspace.com>
2018-08-14 11:37:39 +0200

    rst: insert fig function

--- init-rst.el
+++ init-rst.el
@@ -14,3 +14,16 @@
      (?= simple 0)
      (?- simple 0)
      (?^ simple 0))))
+
+(defun my-rst-insert-fig ()
+  "Insert Sphinx RST figure"
+  (interactive)
+  (insert ".. _figlabel:
+.. figure:: img/figname.png
+    :align: center
+    :width: 600px
+
+    Figure caption
+")
+  )
+(add-hook 'rst-mode-hook (lambda () (local-set-key (kbd "C-c C-f") 'my-rst-insert-fig)))
